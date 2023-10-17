87967916641fc7104520c4ef4d2dc92fc56bf526
AlexNovikova <alexadubinina@mail.ru>
2023-10-17 16:10:27 +0700

    second task

--- seminar_2/Program.cs
+++ seminar_2/Program.cs
@@ -9,3 +9,21 @@ if (num1>= num2){
     Console.WriteLine(num1);
 }
 else Console.WriteLine(num2);
+
+int n = new Random().Next(100, 1000);
+Console.WriteLine(n);
+int last = n%10;
+int first = n/100;
+int res = first*10+last;
+Console.WriteLine(res);
+Console.WriteLine("Enter first number");
+int firstN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second  number");
+int secondN = Convert.ToInt32(Console.ReadLine());
+int ost = firstN%secondN;
+if(ost==0){
+    Console.WriteLine("кратно");
+}
+else {
+    Console.WriteLine($"не кратно, остаток {ost}");
+}

4e3e5ee4d12984f2aefd64d4329e03c6095e9f2d
AlexNovikova <alexadubinina@mail.ru>
2023-10-17 14:36:38 +0700

    seminar 2 task1

---
+++ seminar_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter number");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter number");
+int q = Convert.ToInt32(Console.ReadLine());
+int result = num * num;
+if (q == result)
+{
+    Console.WriteLine("Yes");
+}
+else Console.WriteLine("No");
---
+++ seminar_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int num1 = num % 10;
+int num2 = num/10;
+if (num1>= num2){
+    Console.WriteLine(num1);
+}
+else Console.WriteLine(num2);

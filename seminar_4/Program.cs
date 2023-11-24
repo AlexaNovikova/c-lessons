abf3422e76366f0a3d6b4fe4be6d20bec4cc436e
AlexNovikova <alexadubinina@mail.ru>
2023-11-24 12:17:30 +0700

    senimar 7

---
+++ seminar_4/Program.cs
@@ -0,0 +1,110 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int[,] table = new int[2,5];
+
+void PrintArray(int [,] table)
+{
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        Console.Write($"{table[i, j]} ");
+    }
+    Console.WriteLine();
+}
+}
+
+
+void FillArray(int [,] table)
+{
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        table[i, j] = new Random().Next(1, 10);
+    }
+}
+}
+
+void PrintImage(int [,] image)
+{
+for (int i = 0; i < image.GetLength(0); i++)
+{
+    for (int j = 0; j < image.GetLength(1); j++)
+    {
+        if(image[i,j]==0){
+         Console.Write(" ");
+        }
+        else {
+        Console.Write("+");
+        }
+    }
+    Console.WriteLine();
+}
+}
+
+
+
+int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+
+void FillImage(int[,] image, int row, int col)
+{
+if(image[row,col] == 0) {
+    image[row, col] = 1;
+    FillImage(image, row-1, col);
+    FillImage(image, row, col-1);
+    FillImage(image, row+1, col);
+    FillImage(image, row, col+1);
+}
+}
+
+double fact (double n){
+    if(n==1 || n ==0 ){
+        return 1;
+    }
+    else return n*fact(n-1);
+}
+
+int Fibonacci (int n){
+    if( n==1 || n ==2 ){
+        return 1;
+    }
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+
+FillImage(pic, 16, 16);
+PrintImage(pic);
+
+Console.WriteLine(fact(3));
+
+Console.WriteLine(fact(40));
+
+
+   Console.WriteLine(Fibonacci(30));
+
